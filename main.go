64efde6e046a7c00c99e6b8444b2382ebcaede83
Nicks-code-space <rcravistein@yahoo.in>
2023-06-21 10:31:26 -0400

    fix: handle timeout errors in grpc client

--- main.go
+++ main.go
@@ -14,3 +14,4 @@ if err != nil {
 log.Info().Str("method", r.Method).Msg("request received")
 rows, err := db.QueryContext(ctx, query, args...)
 log.Info().Str("method", r.Method).Msg("request received")
+log.Info().Str("method", r.Method).Msg("request received")
