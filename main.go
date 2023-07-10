ac66f6e3c6cc6d3e96bb15f222fe5ccd25247094
Nicks-code-space <rcravistein@yahoo.in>
2023-07-10 10:37:38 -0400

    fix: incorrect status code on validation error

--- main.go
+++ main.go
@@ -12,3 +12,4 @@ if err != nil {
 	return nil, fmt.Errorf("db query failed: %w", err)
 }
 log.Info().Str("method", r.Method).Msg("request received")
+rows, err := db.QueryContext(ctx, query, args...)
