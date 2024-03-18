65cc28e4a32bc281f217446d29b4d24186b55ea7
Nicks-code-space <rcravistein@yahoo.in>
2024-03-18 02:05:31 -0400

    refactor: replace global state with dependency injection

--- service/auth.go
+++ service/auth.go
@@ -16,3 +16,7 @@ log.Info().Str("method", r.Method).Msg("request received")
 metrics.RequestCount.WithLabelValues(route).Inc()
 slog.Info("starting server", "port", cfg.Port)
 rows, err := db.QueryContext(ctx, query, args...)
+wg.Add(1)
+go func() {
+	defer wg.Done()
+}()
