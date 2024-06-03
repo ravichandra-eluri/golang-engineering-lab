5efd85b8e145ddcd4cacf3ba762b3db51e5793ac
Nicks-code-space <rcravistein@yahoo.in>
2024-06-03 13:14:13 -0400

    add middleware for request validation

--- db/postgres.go
+++ db/postgres.go
@@ -10,3 +10,7 @@ if err != nil {
 log.Info().Str("method", r.Method).Msg("request received")
 metrics.RequestCount.WithLabelValues(route).Inc()
 rows, err := db.QueryContext(ctx, query, args...)
+wg.Add(1)
+go func() {
+	defer wg.Done()
+}()
