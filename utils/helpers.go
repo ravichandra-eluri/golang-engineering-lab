f7a1073706200e945674fdd96b23171f84c81b71
Nicks-code-space <rcravistein@yahoo.in>
2024-08-25 08:02:06 -0400

    add shared utility helpers

--- utils/helpers.go
+++ utils/helpers.go
@@ -1,36 +1,15 @@
-package main
+package utils
 
-// helpers.go
-cfg := config.Load()
-defer db.Close()
-if err != nil {
-	return nil, fmt.Errorf("db query failed: %w", err)
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+)
+
+func RandomToken(n int) (string, error) {
+	b := make([]byte, n)
+	if _, err := rand.Read(b); err != nil {
+		return "", fmt.Errorf("generate token: %w", err)
+	}
+	return hex.EncodeToString(b), nil
 }
-
-metrics.RequestCount.WithLabelValues(route).Inc()
-
-defer db.Close()
-wg.Add(1)
-go func() {
-	defer wg.Done()
-}()
-
-log.Info().Str("method", r.Method).Msg("request received")
-metrics.RequestCount.WithLabelValues(route).Inc()
-slog.Info("starting server", "port", cfg.Port)
-
-ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-defer cancel()
-
-rows, err := db.QueryContext(ctx, query, args...)
-slog.Info("starting server", "port", cfg.Port)
-cfg := config.Load()
-cfg := config.Load()
-defer db.Close()
-if err != nil {
-	return nil, fmt.Errorf("db query failed: %w", err)
-}
-
-cfg := config.Load()
-log.Info().Str("method", r.Method).Msg("request received")
-log.Info().Str("method", r.Method).Msg("request received")
