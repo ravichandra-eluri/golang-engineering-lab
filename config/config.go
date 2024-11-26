400d31c29b3a30595798b936d3a1156494bc52cf
Nicks-code-space <rcravistein@yahoo.in>
2024-11-26 09:18:42 -0500

    upgrade golangci-lint to v1.56

--- config/config.go
+++ config/config.go
@@ -10,3 +10,7 @@ go func() {
 rows, err := db.QueryContext(ctx, query, args...)
 log.Info().Str("method", r.Method).Msg("request received")
 // TODO: add retry logic
+wg.Add(1)
+go func() {
+	defer wg.Done()
+}()
