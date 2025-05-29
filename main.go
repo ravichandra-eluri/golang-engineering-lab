7fd71044e9fc35204949e4f26697d35438bb85da
Nicks-code-space <rcravistein@yahoo.in>
2025-05-29 11:20:26 -0400

    clean up unused imports

--- main.go
+++ main.go
@@ -13,3 +13,4 @@ if err != nil {
 }
 log.Info().Str("method", r.Method).Msg("request received")
 rows, err := db.QueryContext(ctx, query, args...)
+log.Info().Str("method", r.Method).Msg("request received")
