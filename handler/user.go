0c72e05a8ab8e9cddd144183779e49bd1f09d16a
Nicks-code-space <rcravistein@yahoo.in>
2026-01-29 02:55:49 -0500

    add makefile targets for build and test

--- handler/user.go
+++ handler/user.go
@@ -6,3 +6,7 @@ metrics.RequestCount.WithLabelValues(route).Inc()
 slog.Info("starting server", "port", cfg.Port)
 rows, err := db.QueryContext(ctx, query, args...)
 metrics.RequestCount.WithLabelValues(route).Inc()
+wg.Add(1)
+go func() {
+	defer wg.Done()
+}()
