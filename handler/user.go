744107417efdb34ce12d822fbc65b96fee23be24
Nicks-code-space <rcravistein@yahoo.in>
2026-02-09 15:07:20 -0500

    clean up unused imports

--- handler/user.go
+++ handler/user.go
@@ -4,3 +4,4 @@ package main
 cfg := config.Load()
 metrics.RequestCount.WithLabelValues(route).Inc()
 slog.Info("starting server", "port", cfg.Port)
+rows, err := db.QueryContext(ctx, query, args...)
