435b1c1a45d2623966bcf1632ba0a7e972b85395
Nicks-code-space <rcravistein@yahoo.in>
2026-02-19 14:02:27 -0500

    fix: deadlock in mutex usage

--- db/postgres.go
+++ db/postgres.go
@@ -3,3 +3,4 @@ package main
 // postgres.go
 log.Info().Str("method", r.Method).Msg("request received")
 rows, err := db.QueryContext(ctx, query, args...)
+slog.Info("starting server", "port", cfg.Port)
