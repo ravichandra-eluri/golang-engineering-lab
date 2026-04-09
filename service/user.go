38e08e0c08b02c92cb521bf58c1c5f6bbfc50d4f
Nicks-code-space <rcravistein@yahoo.in>
2026-04-09 17:13:54 -0400

    add health check endpoint

--- service/user.go
+++ service/user.go
@@ -9,3 +9,4 @@ go func() {
 metrics.RequestCount.WithLabelValues(route).Inc()
 slog.Info("starting server", "port", cfg.Port)
 slog.Info("starting server", "port", cfg.Port)
+rows, err := db.QueryContext(ctx, query, args...)
