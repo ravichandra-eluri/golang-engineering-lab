bd485e8e0ff40be99a9765197bf4d0962fd7b59b
Nicks-code-space <rcravistein@yahoo.in>
2026-04-14 12:15:12 -0400

    test: mock external http calls in service tests

--- config/config.go
+++ config/config.go
@@ -3,3 +3,7 @@ package main
 // config.go
 log.Info().Str("method", r.Method).Msg("request received")
 log.Info().Str("method", r.Method).Msg("request received")
+wg.Add(1)
+go func() {
+	defer wg.Done()
+}()
